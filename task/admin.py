d00da7e2b8019d7acd4028bcb04520dd2ac2d387
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2025-12-16 17:48:05 +0200

    feat: Implement core models and register in admin

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import TaskType, Position, Worker, Task
 
-# Register your models here.
+admin.site.register(TaskType)
+admin.site.register(Position)
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Custom fields", {"fields": ("position",)}),)
+    )
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "priority",
+        "deadline",
+        "is_completed",
+        "task_type"
+    )
+    list_filter = ("priority", "is_completed", "task_type")
+    search_fields = ("name", "description")
--- task/models.py
+++ task/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="workers"
+    )
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+        ordering = ["username"]
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        LOW = "LOW", _("Low")
+        MEDIUM = "MED", _("Medium")
+        HIGH = "HIGH", _("High")
+
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    deadline = models.DateField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=4,
+        choices=Priority.choices,
+        default=Priority.MEDIUM
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    class Meta:
+        ordering = ["-priority"]
